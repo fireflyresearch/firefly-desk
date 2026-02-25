96d86a4e22e16fb78b5ead562a9f1b55656102eb
Andrés Contreras Guillén <me@ancongui.com>
2026-02-25 11:18:03 +0100

    feat: add Help & Guides admin page with two-pane doc viewer

---
+++ frontend/src/routes/(app)/admin/help/+page.svelte
@@ -0,0 +1,11 @@
+<!--
+  Admin - Help & Guides page.
+
+  Copyright 2026 Firefly Software Solutions Inc. All rights reserved.
+  Licensed under the Apache License, Version 2.0.
+-->
+<script lang="ts">
+	import HelpGuidesPage from '$lib/components/admin/HelpGuidesPage.svelte';
+</script>
+
+<HelpGuidesPage />
